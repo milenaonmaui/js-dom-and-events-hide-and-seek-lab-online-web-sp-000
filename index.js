0c0cd9be8657cae8211e5a89730dad4cd449f85d
Milena Kari <milenakari@yahoo.com>
2019-08-24 18:40:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector( '#nested .target' )
+}
+function increaseRankBy(n){
+  const rankedLists = document.querySelectorAll( '.ranked-list' )
+      const firstList = rankedLists[ 0 ]
+      const secondList = rankedLists[ 1 ]
+
+      let children = firstList.children
+      for ( let i = 0, l = children.length; i < l; i++) {
+            children[ i ].innerHTML = parseInt(children[ i ].innerHTML) + n;
+      }
+
+      children = secondList.children
+      for ( let i = 0, l = children.length; i < l; i++) {
+            children[ i ].innerHTML = parseInt(children[ i ].innerHTML) + n;
+      }
+
+}
+
+function deepestChild(){
+   return document.querySelector( '#grand-node div div div div')
+}
