622f4c71567167849646b1255c460f41d0bb4b63
Milena Kari <milenakari@yahoo.com>
2019-08-24 18:38:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,14 +12,12 @@ function increaseRankBy(n){
 
       let children = firstList.children
       for ( let i = 0, l = children.length; i < l; i++) {
-            children[ i ].innerHTML = parseInt;
+            children[ i ].innerHTML = parseInt(children[ i ].innerHTML) + 1;
       }
 
       children = secondList.children
-      start = 12
-
-      for ( let i = 0, l = children.length; i < l; i++ ) {
-        expect( parseInt( children[ i ].innerHTML ) ).to.equal( start - i + 3 )
+      for ( let i = 0, l = children.length; i < l; i++) {
+            children[ i ].innerHTML = parseInt(children[ i ].innerHTML) + 1;
       }
-    } )
+
 }
