30408eeba43644d47c24c58732fb4ea25327926b
Milena Kari <milenakari@yahoo.com>
2019-08-24 18:35:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,20 @@ function nestedTarget(){
   return document.querySelector( '#nested .target' )
 }
 function increaseRankBy(n){
-  
+  const rankedLists = document.querySelectorAll( '.ranked-list' )
+      const firstList = rankedLists[ 0 ]
+      const secondList = rankedLists[ 1 ]
+
+      let children = firstList.children
+      for ( let i = 0, l = children.length; i < l; i++ ) {
+            parseInt( children[ i ].innerHTML )++;
+      }
+
+      children = secondList.children
+      start = 12
+
+      for ( let i = 0, l = children.length; i < l; i++ ) {
+        expect( parseInt( children[ i ].innerHTML ) ).to.equal( start - i + 3 )
+      }
+    } )
 }
