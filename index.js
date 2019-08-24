ccdbe8eea68d2b9f3a40e3098310360916790b5c
Milena Kari <milenakari@yahoo.com>
2019-08-24 18:27:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
-  let el1 = document.getElementById('nested')
-  return el1.getElementsByClassName('target')
+  return document.querySelector( '#nested .target' )
 }
